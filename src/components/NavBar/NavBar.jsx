44b26c1d9809ca60d6c2ec7ea9f685fcfca2516c
Your Name <you@example.com>
2023-06-29 17:43:25 -0300

    otro commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,10 +6,12 @@ import CartWidget from '../CartWidget/CartWidget';
 export const NavBar = () => {
   return (
     <header>
-    <h1>The Shoe Store</h1>
+    <h1 className='titulo'>The Shoe Store</h1>
+    <img className='logoinicio' src='https://i.pinimg.com/originals/64/31/ed/6431eda72c9929b1b28405c2df11be19.jpg' alt='logo'/>
 
     <nav>
         <ul>
+            <li>Inicio</li>
             <li>Moda</li>
             <li>Running</li>
             <li>Sandalias</li>
